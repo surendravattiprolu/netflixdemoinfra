71b86f3cbc61e0d037145a6f6e0c927cb8a4286e
surendravattiprolu <balasurendravattiprolu@gmail.com>
2026-06-16 09:29:28 +0530

    Update AMI and security group IDs for AWS instance

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0ecb62995f68bb549"
+  ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "c7i-flex.large"
   key_name               = "rahamdocker"
-  vpc_security_group_ids = ["sg-0ddd6bb55cfb8039c"]
+  vpc_security_group_ids = ["sg-08913df43140c1f63"]
   tags = {
     Name = var.instance_names[count.index]
   }

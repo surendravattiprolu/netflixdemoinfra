4306367088aba7247a2212148a0c46892c7485be
surendravattiprolu <balasurendravattiprolu@gmail.com>
2026-06-16 10:13:21 +0530

    Change key_name for AWS instance resource

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "c7i-flex.large"
-  key_name               = "rahamdocker"
+  key_name               = "TERRAFORM"
   vpc_security_group_ids = ["sg-08913df43140c1f63"]
   tags = {
     Name = var.instance_names[count.index]
